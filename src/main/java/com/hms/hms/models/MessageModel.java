daa1deafcc90ce428b97f90551b22741d76eba90
jakubgoc <jakubgoc98@gmail.com>
2021-01-13 18:26:10 +0100

    .

--- src/main/java/com/hms/hms/models/MessageModel.java
+++ src/main/java/com/hms/hms/models/MessageModel.java
@@ -1,60 +1,8 @@
 package com.hms.hms.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-import java.util.Date;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-@Document
 public class MessageModel {
 
-    @Id
-    private String id;
-    private String chatId;
-    private String senderId;
-    private String recipientId;
-    private String senderName;
-    private String recipientName;
-    private String content;
-    private Date timestamp;
-    private MessageStatus status;
-
-    public MessageStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(MessageStatus status) {
-        this.status = status;
-    }
-
-    public String getRecipientId() {
-        return recipientId;
-    }
-
-    public String getSenderId() {
-        return senderId;
-    }
-
-    public Object getId() {
-        return id;
-    }
-
-    public Object getSenderName() {
-        return senderName;
-    }
-
-    public void setChatId(String s) {
-        this.chatId=chatId;
-    }
-
-    /*private String message;
+    private String message;
     private String fromLogin;
 
     @Override
@@ -79,5 +27,5 @@ public class MessageModel {
 
     public void setFromLogin(String fromLogin) {
         this.fromLogin = fromLogin;
-    }*/
+    }
 }
